18b80d4a08e8bf2dd019093a5369561dad5c90a8
dabl20 <60107999+dabl20@users.noreply.github.com>
2020-04-08 12:37:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var baseUrl = 'https://shikimori.one';
-
-fetch(baseUrl + `/api/animes?&limit=50&status=latest`)
+fetch(baseUrl + `/api/animes?&limit=15&status=latest`)
   .then(response => response.json())
   .then(data => {
     data.forEach(element => {
@@ -12,9 +11,20 @@ document.forms[0].addEventListener('submit', function (event) {
   event.preventDefault();
   document.getElementById('content').innerHTML = '';
   let text = document.forms[0].querySelector('input[type="text"]').value;
-  contentRender(text);
+  search(text);
   this.reset();
 })
+
+function search(text) {
+  let option
+  fetch(baseUrl + `/api/animes?&rating=g&limit=15&search=${text}`)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(element => {
+        addItem(element);
+      });
+    });
+}
 
 function addItem(data) {
   let item = document.createElement('div');
@@ -25,6 +35,30 @@ function addItem(data) {
   itemWrapper.className = 'itemWrapper';
   item.append(itemWrapper);
 
+  itemWrapper.addEventListener('click', function (event) {
+    let popup = document.createElement('div');
+    popup.id = 'popup';
+    root.before(popup);
+
+    let popupWrapper = document.createElement('div');
+    popupWrapper.classList = 'popupWrapper wrapper';
+    popup.append(popupWrapper);
+
+    let delPopup = document.createElement('span');
+    delPopup.classList = 'delPopup fas fa-times';
+    popupWrapper.append(delPopup);
+
+    delPopup.addEventListener('click', function() {
+      popup.remove();
+    })
+
+    fetch(baseUrl + `/api/animes/${data.id}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      });
+  })
+
   let imageWrapper = document.createElement('div');
   imageWrapper.className = 'imageWrapper';
   itemWrapper.append(imageWrapper);
@@ -34,20 +68,10 @@ function addItem(data) {
   imageWrapper.append(img);
 
   let name = document.createElement('h3');
-  if (data.russian == '') {
-    name.textContent = `${data.name}`;
-  } else {
+  if (data.russian !== '') {
     name.textContent = `${data.russian}`;
+  } else {
+    name.textContent = `${data.name}`;
   }
   itemWrapper.append(name);
-}
-
-function contentRender(text) {
-  fetch(baseUrl + `/api/animes?&limit=50&search=${text}`)
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(element => {
-        addItem(element);
-      });
-    });
 }
